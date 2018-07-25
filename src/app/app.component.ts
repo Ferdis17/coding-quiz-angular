517439762d5184575ea270a7e8317419c3933528
Ferdis17 <fmuriyesu@users.noreply.github.com>
2018-07-25 11:22:03 -0400

    whole commit
    The whole commit after developing single page app with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { TaskService } from './service/task.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  taskList = [];
+  taskForm: FormGroup;
+  formSubmitted: boolean;
+
+  constructor(private taskService: TaskService) {
+  }
+
+  ngOnInit() {
+    this.taskService.getTaskData().then( res => {
+      this.taskList = res;
+    });
+    this.generateForm();
+  }
+
+  generateForm(): void {
+    this.taskForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      date: new FormControl('', [Validators.required, this.dateValidator]),
+      assigned: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onSubmit() {
+    // reformat html5 input date to match existing model
+    const inputDate = this.taskForm.value.date;
+    const dateArr = inputDate.split('-');
+    this.taskForm.value.date =   dateArr[1] + '/' + dateArr[2] + '/' + dateArr[0];
+    this.taskService.addTask(this.taskForm.value);
+    this.formSubmitted = true;
+    this.generateForm();
+  }
+
+  dateValidator(control: FormControl): { [key: string]: boolean } {
+    const isValidDate = Date.parse(control.value);
+    if (isNaN(isValidDate)) {
+      return { 'dateInvalid': false };
+    }
+    return null;
+  }
+
 }
